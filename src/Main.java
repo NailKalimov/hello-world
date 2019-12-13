30113d2eac982c86d87593fe4e430028b36546e4
Nail <nailkalimov@gmail.com>
2019-12-14 02:22:59 +0300

    concatenation

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        String hello="Hello, ";
+        String world="World";
+        System.out.println(hello + world + "!");
     }
 }
